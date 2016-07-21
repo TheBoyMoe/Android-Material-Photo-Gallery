2540cec6f5e928dab8a18f76ffa05c7dff67a527
billfero <billfero@gmail.com>
2016-07-21 11:19:16 +0100

    impl navigation view

--- app/src/main/java/com/example/materialphotogallery/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/materialphotogallery/ui/activity/MainActivity.java
@@ -3,9 +3,17 @@ package com.example.materialphotogallery.ui.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.example.materialphotogallery.R;
 import com.example.materialphotogallery.common.Utils;
@@ -17,11 +25,21 @@ public class MainActivity extends AppCompatActivity {
         activity.startActivity(intent);
     }
 
+    private CoordinatorLayout mLayout;
+    private DrawerLayout mDrawer;
+    private NavigationView mNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLayout = (CoordinatorLayout) findViewById(R.id.coordinator_layout);
+
+        initToolbar();
+        initFab();
+        setupDrawerLayout();
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -31,11 +49,59 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.action_about) {
-            Utils.showToast(this, "Clicked about");
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                Utils.showToast(this, "Clicked about");
+                return true;
+            case android.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private void setupDrawerLayout() {
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                // handle clicking on individual nav items via switch()
+                Utils.showSnackbar(mLayout, "Clicked " + item.getTitle());
+
+                item.setChecked(true);
+                mDrawer.closeDrawers();
+                return true;
+            }
+        });
+    }
+
+    private void initFab() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        if (fab != null) {
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Utils.showSnackbar(mLayout, "Clicked on fab");
+                }
+            });
+        }
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            ActionBar actionbar = getSupportActionBar();
+            if (actionbar != null) {
+                actionbar.setHomeAsUpIndicator(R.drawable.ic_drawer);
+                actionbar.setDisplayHomeAsUpEnabled(true);
+            }
+        }
+
+
+    }
+
 
 
 }
